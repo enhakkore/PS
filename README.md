eb4e18e7a6fa7397dcb656f34d03ef33d0be429c
enhakkore <ahn.ephraim@gmail.com>
2019-12-15 14:38:48 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PS
+# PS  
+### 프로그래머스  
+* [타켓 넘버](https://programmers.co.kr/learn/courses/30/lessons/43165)
---
+++ programmers/타켓_넘버/타켓_넘버.java
@@ -0,0 +1,39 @@
+class Solution {
+    static int limit = 0;
+    static int target = 0;
+    static int ans = 0;
+    static int[] numbers;
+    
+    static int[] r = {1, -1};
+
+    public void sol1(int order, int order1, int value){
+        if(order == limit) {
+            if(target == value){
+                // System.out.println(value);
+                ans++;
+            }
+            return ;
+        }
+
+        int tmp = numbers[order]*r[order1];
+        tmp += value;
+
+        if(order1 == 0){
+            sol1(order+1, order1, tmp);
+            sol1(order, order1+1, value);
+        }
+        else {
+            sol1(order+1, order1-1, tmp);
+        }
+    }
+
+    public int solution(int[] numbers, int target) {
+        limit = numbers.length;
+        this.target = target;
+        this.numbers = numbers;
+
+        sol1(0, 0, 0);
+
+        return ans;
+    }
+}
