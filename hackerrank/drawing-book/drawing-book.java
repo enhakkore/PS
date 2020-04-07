05ce4ebfd85e45beca98519456be52866fa7c5b9
enhakkore <ahn.ephraim@gmail.com>
2020-04-07 13:21:35 +0900

    hackerrank 문제 추가

---
+++ hackerrank/drawing-book/drawing-book.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+        int count = 0;
+
+        if((n-p)/2 >= p/2){
+            int page = 1;
+            while(true){
+                if(page >= p) break;
+                page += 2;
+                count++;
+            }
+        } else {
+            int page = n % 2 == 0 ? n : n-1;
+            while(true){
+                if(page <= p) break;
+                page -= 2;
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
