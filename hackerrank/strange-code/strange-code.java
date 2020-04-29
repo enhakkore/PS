a217cbe005263a037cd4823f016cfc51ca6234b7
enhakkore <ahn.ephraim@gmail.com>
2020-04-29 11:58:06 +0900

    hackerrank 문제 추가

---
+++ hackerrank/strange-code/strange-code.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+        long sum = 0L;
+        long mid = 3L;
+
+        while(true){
+            sum += mid;
+
+            if(sum >= t) break;
+
+            mid *= 2;
+        }
+
+        long start = sum - mid + 1;
+        long diff = t - start;
+
+        return mid - diff;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
