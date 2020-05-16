98be8a1e770c515b3e7e75f6fdf005b780404686
enhakkore <ahn.ephraim@gmail.com>
2020-05-16 13:10:30 +0900

    hackerrank 문제 추가

---
+++ hackerrank/the-love-letter-mystery/the-love-letter-mystery.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the theLoveLetterMystery function below.
+    static int theLoveLetterMystery(String s) {
+        int limit = s.length()/2;
+        char[] sArr = s.toCharArray();
+
+        int min_num = 0;
+
+        for(int i = 0 ; i < limit ; i++ ){
+            min_num += Math.abs(sArr[i]-sArr[sArr.length-i-1]);
+        }
+
+        return min_num;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = theLoveLetterMystery(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
