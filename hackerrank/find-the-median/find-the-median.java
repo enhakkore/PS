64d5dd6f707c4500b5d2b2152f5db3a0495dc34b
enhakkore <ahn.ephraim@gmail.com>
2020-05-17 14:49:42 +0900

    hackerrank 문제 추가

---
+++ hackerrank/find-the-median/find-the-median.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findMedian function below.
+    static int findMedian(int[] arr) {
+        Map<Integer, Integer> info = new TreeMap<>();
+
+        Arrays.stream(arr).forEach( n -> {
+            int count = info.getOrDefault(n, 0);
+            info.put(n, count+1);
+        });
+
+        int ans = 0;
+        long sum = 0L;
+        int base = arr.length/2;
+        for(int k : info.keySet()){
+            sum += info.get(k);
+            if(sum > base){
+                ans = k;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = findMedian(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
